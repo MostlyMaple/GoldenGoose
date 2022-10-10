f84480d38fe4dcc03ef11bd1a98fd2463178b034
Stephen Dean <kdt298@my.utsa.edu>
2022-10-10 17:33:09 -0500

    new 2

--- victim21/execute.py
+++
@@ -1,5 +0,0 @@
-import os
-
-for i in range(100):
-    bashCommand = "python3 ./goldengoose.py " + str(i) + " | nc victim21 79"
-    os.system(bashCommand)
---
+++ victim21/execute.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+for i in {1..5}
+do
+   python3 goldengoose.py $i
+done
